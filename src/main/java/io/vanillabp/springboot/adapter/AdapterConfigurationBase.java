d05f8dc3f18ee553191697832efd5237daf29b08
Stephan Pelikan <stephan.pelikan@phactum.at>
2023-02-01 16:25:15 +0100

    Support more than field injection for ProcessService

--- src/main/java/io/vanillabp/springboot/adapter/AdapterConfigurationBase.java
+++ src/main/java/io/vanillabp/springboot/adapter/AdapterConfigurationBase.java
@@ -1,9 +1,9 @@
 package io.vanillabp.springboot.adapter;
 
 import org.springframework.beans.factory.InjectionPoint;
-import org.springframework.core.ResolvableType;
 import org.springframework.data.repository.CrudRepository;
 
+import java.lang.reflect.ParameterizedType;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
@@ -25,12 +25,23 @@ public abstract class AdapterConfigurationBase<P extends ProcessServiceImplement
             final InjectionPoint injectionPoint,
             final BiFunction<CrudRepository<DE, String>, Class<DE>, P> processServiceBeanSupplier) throws Exception {
         
-        final var resolvableType = ResolvableType.forField(injectionPoint.getField());
-
-        final var workflowAggregateClass = (Class<DE>) resolvableType
-                .getGeneric(0)
-                .resolve();
-
+        final ParameterizedType processServiceGenericType; 
+        if (injectionPoint.getMethodParameter() != null) {
+            processServiceGenericType = 
+                    (ParameterizedType) injectionPoint
+                    .getMethodParameter()
+                    .getGenericParameterType();
+        } else if (injectionPoint.getField() != null) {
+            processServiceGenericType =
+                    (ParameterizedType) injectionPoint
+                    .getField()
+                    .getGenericType();
+        } else {
+            throw new RuntimeException("Unsupported injection of ProcessService, only field-, constructor- and method-parameter-injection allowed!");
+        }
+        final Class<DE> workflowAggregateClass = (Class<DE>)
+                processServiceGenericType.getActualTypeArguments()[0];
+        
         final var existingService = connectableServices.get(workflowAggregateClass);
         if (existingService != null) {
             return (P) existingService;
